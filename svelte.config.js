d6fe3945b724e0f5fd4f2c50b5bc2615d2b15e86
Leonardo <22091005@alumno.utmetropolitana.edu.mx>
2026-04-25 20:58:14 -0600

    Fix: limpiar adapters duplicados

--- svelte.config.js
+++ svelte.config.js
@@ -14,14 +14,3 @@ export default {
   }
 };
 
-import adapter from '@sveltejs/adapter-static';
-
-export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
-    })
-  }
-};
